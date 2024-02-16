37a96bea1c8433b9c0af6650fcd5e0be803eb0df
diegodev9 <diegodev9@gmail.com>
2024-02-16 11:41:33 -0300

    bug fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,7 +24,7 @@ class ProductsController < ApplicationController
 =end
 
     @categories = Category.order(name: :asc).load_async
-    @pagy, @products = pagy_countless(FindProducts.new.call(params), items: 12)
+    @pagy, @products = pagy_countless(FindProducts.new.call(product_params_index), items: 12)
   end
 
   def show
@@ -68,6 +68,10 @@ class ProductsController < ApplicationController
 
   def set_categories
     @categories = Category.category_select
+  end
+
+  def product_params_index
+    params.permit(:category_id, :min_price, :max_price, :query_text, :order_by)
   end
 
   def product_params
