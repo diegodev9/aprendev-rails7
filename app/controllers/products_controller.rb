45925fa9a61e18c7e3090e3687755902a37d3abb
diegodev9 <diegodev9@gmail.com>
2024-02-29 10:56:28 -0300

    agregado actioncable para actualizacion de productos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -33,6 +33,7 @@ class ProductsController < ApplicationController
 
   def update
     if @product.update(product_params)
+      notify_all_users
       redirect_to products_path, notice: t('.updated')
     else
       render :edit, status: :unprocessable_entity
@@ -45,6 +46,15 @@ class ProductsController < ApplicationController
   end
 
   private
+
+  def notify_all_users
+    ActionCable.server.broadcast(
+      "product_#{@product.id}",
+      {
+        action: 'updated'
+      }
+    )
+  end
 
   def set_product
     @product ||= Product.find(params[:id])
