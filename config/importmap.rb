f48697e73ccd4d2d22e3333859b1491ddc264f92
diegodev9 <diegodev9@gmail.com>
2024-02-29 11:08:46 -0300

    refactor de actioncable con turbostreams

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,5 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "tailwindcss-stimulus-components" # @4.0.4
 pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
-pin "@rails/actioncable", to: "@rails--actioncable.js" # @7.1.3
+# pin "@rails/actioncable", to: "@rails--actioncable.js" # @7.1.3
 pin_all_from "app/javascript/controllers", under: "controllers"
